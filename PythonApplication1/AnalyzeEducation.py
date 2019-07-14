211c96715591355de150fde236f6eb5d6bed5223
mejustandrew <mihaivma4c@gmail.com>
2019-07-14 16:12:04 +0300

    Finished AnalyzeEducation.py.

--- PythonApplication1/AnalyzeEducation.py
+++ PythonApplication1/AnalyzeEducation.py
@@ -1,29 +1,41 @@
 import csv
 import os
 
-#ageResultsFilePath = 'C:\\Users\\harry\\Desktop\\statistics\\results\\ageResults.txt'
-#os.remove(ageResultsFilePath)
+educationResultsFilePath ='C:\\Users\\harry\\Desktop\\statistics\\results\\educationResults.txt'
+if os.path.exists(educationResultsFilePath):
+    os.remove(educationResultsFilePath)
 
-def saveAverageAgeFromFile(opinionsFilePath, category):
+def saveEducationLevelFromFile(opinionsFilePath, category):
     with open(opinionsFilePath, 'r', encoding="utf-8") as opinionsFile:
-        sum = 0
-        numberOfValidEntries = 0
+        undergraduateNumber = 0
+        banchelorDegree = 0
+        others = 0
         notMentioned = 0
 
         for line in csv.reader(opinionsFile, quotechar='"', delimiter=',',quoting=csv.QUOTE_ALL, skipinitialspace=True):
-            stringAge = line[77]
+            education = line[8]
 
-            if stringAge != 'NA':
-                age = float(stringAge)
-                sum = sum + age
-                numberOfValidEntries = numberOfValidEntries + 1
-            else:
-               notMentioned = notMentioned + 1
+            if education == 'Bachelor’s degree (BA, BS, B.Eng., etc.)':
+                banchelorDegree += 1
+            if education == 'Associate degree' or \
+                education == 'Some college/university study without earning a degree' or \
+                education == 'Secondary school (e.g. American high school, German Realschule or Gymnasium, etc.)' or \
+                education == 'Primary/elementary school' or \
+                education == 'I never completed any formal education':
+                undergraduateNumber += 1
 
-        averageAge = sum / numberOfValidEntries
-        #with open(ageResultsFilePath, 'a') as resultsFile:
-        #    resultsFile.write('category: ' + category + '  average age: ' + str(averageAge) + \
-        #                      '  valid responses: ' + str(numberOfValidEntries) + '  NAs: ' + str(notMentioned) + '\n')
+            if education == 'Master’s degree (MA, MS, M.Eng., MBA, etc.)' or education == 'Professional degree' or education == 'Doctoral degree':
+                others += 1
+
+            if education == 'NA':
+                notMentioned += 1
+
+        with open(educationResultsFilePath, 'a') as resultsFile:
+            resultsFile.write('category: ' + category + 
+            ' Bachelor degree: ' + str(banchelorDegree) +
+            ' Undergraduate: ' + str(undergraduateNumber) +
+            ' Others: ' + str(others) +
+            ' NAs: ' + str(notMentioned) + '\n')
 
 
 broadlyUsefulFilePath = 'C:\\Users\\harry\\Desktop\\statistics\\broadlyUsed.csv'
@@ -33,10 +45,10 @@ irresponsableWasteFilePath = 'C:\\Users\\harry\\Desktop\\statistics\\irresponsab
 cryptocurrenciesFilePath = 'C:\\Users\\harry\\Desktop\\statistics\\currenciesUsed.csv'
 noOpinionFilePath = 'C:\\Users\\harry\\Desktop\\statistics\\noOpinion.csv'
 
-saveAverageAgeFromFile(broadlyUsefulFilePath, 'broadly used')
-saveAverageAgeFromFile(immutabilityUsefulFilePth, 'immutability')
-saveAverageAgeFromFile(passingFudFilePath, 'passing fud')
-saveAverageAgeFromFile(irresponsableWasteFilePath, 'irresponsable waste')
-saveAverageAgeFromFile(cryptocurrenciesFilePath, 'cryptocurrencies')
-saveAverageAgeFromFile(noOpinionFilePath, 'no opinion')
+saveEducationLevelFromFile(broadlyUsefulFilePath, 'broadly used')
+saveEducationLevelFromFile(immutabilityUsefulFilePth, 'immutability')
+saveEducationLevelFromFile(passingFudFilePath, 'passing fud')
+saveEducationLevelFromFile(irresponsableWasteFilePath, 'irresponsable waste')
+saveEducationLevelFromFile(cryptocurrenciesFilePath, 'cryptocurrencies')
+saveEducationLevelFromFile(noOpinionFilePath, 'no opinion')
         
